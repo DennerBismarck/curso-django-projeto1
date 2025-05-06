ad52d073fa6c6b7bb3f828d2f29bf4f76a599800
DennerBismarck <dennerbismarck@gamil.com>
2025-05-06 15:47:12 -0300

    feat: creating recipe url

--- recipes/views.py
+++ recipes/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={'name': 'Denner Bismarck'})
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/home.html', context={'name': 'Denner Bismarck'})
